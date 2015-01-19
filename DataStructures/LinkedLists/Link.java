e198de7a10995376c19aba5b54e306b6a23d7c8a
CarlW194 <carl.white194@gmail.com>
2015-01-19 22:58:22 +0000

    Link.java
    This program will add the word to the link and will give all the data in the link to the linkedlist class to display.

---
+++ DataStructures/LinkedLists/Link.java
@@ -0,0 +1,17 @@
+package LinkedLists;
+
+public class Link { 
+	 public String data;
+	 public Link next; 
+	 public Link previous; 
+
+	 public Link(String word){
+		 this.data = word;
+	 }
+	 
+	 
+	 public String displayLink(){
+		 return data;
+	 }
+
+}
